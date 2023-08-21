8963cc1adafe337f22953905c096c2548b473426
IgorOliverX <igorferoli019@gmail.com>
2023-08-21 17:01:04 -0300

    commyta e cmcsac

--- js/home.js
+++ js/home.js
@@ -71,3 +71,94 @@ document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const addToCartButtons = document.querySelectorAll(".button-hover-background");
+    const cartItemsList = document.querySelector(".cart-items");
+    const cartTotalSpan = document.querySelector(".cart-total");
+    const cartSidebar = document.querySelector(".cart-sidebar");
+    const cartAddButton = document.getElementById("openCartSideBar")
+
+    cartAddButton.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        if (cartSidebar.style.right === "0px") {
+            cartSidebar.style.right = "-300px"; 
+        } else {
+            cartSidebar.style.right = "0"; 
+        }
+    });
+    
+    let cartTotal = 0;
+    
+    addToCartButtons.forEach(button => {
+      button.addEventListener("click", function () {
+        const productCard = button.closest(".event-card");
+        const productName = productCard.querySelector(".product-title").innerText;
+        const productPrice = parseFloat(productCard.querySelector(".product-price").innerText.replace("R$", ""));
+        
+        const cartItem = document.createElement("li");
+        cartItem.classList.add("cart-item");
+        cartItem.innerHTML = `
+          <span>${productName}</span>
+          <span>R$${productPrice.toFixed(2)}</span>
+          <button class="remove-from-cart">Remover</button>
+        `;
+        
+        cartItemsList.appendChild(cartItem);
+        cartTotal += productPrice;
+        cartTotalSpan.innerText = `R$${cartTotal.toFixed(2)}`;
+        
+        cartSidebar.style.right = "0";
+        
+        const removeButtons = cartItemsList.querySelectorAll(".remove-from-cart");
+        removeButtons.forEach(removeButton => {
+          removeButton.addEventListener("click", function () {
+            const itemToRemove = removeButton.closest(".cart-item");
+            const itemPrice = parseFloat(itemToRemove.querySelector("span:nth-child(2)").innerText.replace("R$", ""));
+            
+            cartItemsList.removeChild(itemToRemove);
+            cartTotal -= itemPrice;
+            cartTotalSpan.innerText = `R$${cartTotal.toFixed(2)}`;
+            
+            if (cartItemsList.children.length === 0) {
+              cartSidebar.style.right = "-300px";
+            }
+          });
+        });
+      });
+    });
+
+  
+    
+    cartAddButton.forEach(cartButton =>{ 
+    cartButton.addEventListener("click", function(){
+        cartSidebar.style.right = "0";
+    })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+  
