e2bb8807d34498ffbafad9e937d04a438501ce22
Vinícius <107704540+epicestudar@users.noreply.github.com>
2023-08-21 19:35:43 +0000

    commita y commita

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 const wrapper = document.querySelector(".wrapper");
-const carousel = document.querySelector(".carousel");
+const carousel = document.querySelector(".carouselProducts");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const carouselChildrens = [...carousel.children];
